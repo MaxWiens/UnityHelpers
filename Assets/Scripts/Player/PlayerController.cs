e529f57d07ee4c61fe007f5f7a1b82fc9d36cdf5
Max <maxawiens@gmail.com>
2021-02-26 02:30:29 -0600

    added wall collision adjustment and fixed ceiling detection

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,22 +11,24 @@ public class PlayerController : MonoBehaviour {
 	[SerializeField, NotNull] private Rigidbody _rigidBody;
 
 	public void Move(Vector2 direction){
-		Vector3 adjustedGroundVector = Vector3.left;
-		Vector3 normalCrossUp = Vector3.Cross(Vector3.up,_groundNormal).normalized;
-		Vector3 slopeDirection = Vector3.Cross(_groundNormal,normalCrossUp).normalized;
-
-		Vector3 adjustedMoveVec = Vector3.zero;
 		if(direction.x != 0 || direction.y != 0){
+			Vector3 adjustedMoveVec = Vector3.zero;
 			if(_groundNormal.x == 0 && _groundNormal.z == 0f){
 				adjustedMoveVec = new Vector3(direction.x, 0f, direction.y);
 			}else{
+				Vector3 normalCrossUp = Vector3.Cross(Vector3.up,_groundNormal).normalized;
+				Vector3 slopeDirection = Vector3.Cross(_groundNormal,normalCrossUp).normalized;
 				float theata = Vector2.SignedAngle(direction, new Vector2(slopeDirection.x,slopeDirection.z))+90;
 				Quaternion q = Quaternion.AngleAxis(theata, _groundNormal);
 				adjustedMoveVec = q * normalCrossUp;
 			}
+
+			if(_wallNormal.x != 0f || _wallNormal.y != 0f ||_wallNormal.z != 0f)
+				adjustedMoveVec = _wallNormal+adjustedMoveVec;
+			_rigidBody.AddForce(adjustedMoveVec*20f);
 		}
 		//Debug.DrawRay(transform.position, adjustedMoveVec, Color.cyan);
-		_rigidBody.AddForce(adjustedMoveVec*20f);
+
 	}
 
 	private Vector2 _moveVec = new Vector2();
@@ -65,8 +67,10 @@ public class PlayerController : MonoBehaviour {
 				_groundingObjects.Add(other.gameObject);
 				Debug.Log($"new normal {f}");
 				break;
-			}else if(f <= 360 - MaxSlope){
+			}else if(f <= 180 - MaxSlope){
 				Debug.Log($"wall normal? {f}");
+				_wallNormal= contactPoint.normal;
+				_wallObjects.Add(other.gameObject);
 			}else{
 				Debug.Log($"celing normal? {f}");
 			}
@@ -81,19 +85,18 @@ public class PlayerController : MonoBehaviour {
 			bool isGood = false;
 			foreach(ContactPoint contactPoint in other.contacts){
 				float f = Vector3.Angle(contactPoint.normal, Vector3.up);
-
-
 				if(f <= MaxSlope){
-
-
 					// is floor
 					_groundNormal = contactPoint.normal;
 					_groundingObjects.Add(other.gameObject);
 					isGood = true;
 					break;
+				}else if(f <= 180 - MaxSlope){
+					//Debug.Log($"wall normal? {f}");
+					_wallNormal= contactPoint.normal;
+					_wallObjects.Add(other.gameObject);
 				}else{
-					// is wall or celing
-					//_groundingObjects
+					//Debug.Log($"celing normal? {f}");
 				}
 			}
 			if(!isGood){
@@ -105,10 +108,18 @@ public class PlayerController : MonoBehaviour {
 
 	private HashSet<GameObject> _groundingObjects = new HashSet<GameObject>();
 	private HashSet<GameObject> _wallObjects = new HashSet<GameObject>();
+	private Vector3 _wallNormal = Vector3.zero;
 
 	private void OnCollisionExit(Collision other) {
-		_groundingObjects.Remove(other.gameObject);
-		if(_groundingObjects.Count == 0)
-			_groundNormal = Vector3.up;
+
+		if(_groundingObjects.Remove(other.gameObject)){
+			if(_groundingObjects.Count == 0)
+				_groundNormal = Vector3.up;
+			return;
+		}else if(_wallObjects.Remove(other.gameObject)){
+			if(_wallObjects.Count == 0)
+				_wallNormal = Vector3.zero;
+			return;
+		}
 	}
 }
